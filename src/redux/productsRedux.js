b8242780b6f83c9df8a28ed6486410dd87f710b6
Jakub Kozik <jakubkozik120@gmail.com>
2022-06-10 19:24:03 +0200

    Add styles to compare button. Add compare state in initialState -> products

--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -3,6 +3,8 @@ export const getAll = ({ products }) => products;
 export const getCount = ({ products }) => products.length;
 export const getIsFavorite = ({ products }) =>
   products.filter(product => product.isFavorite === true);
+export const getCompare = ({ products }) =>
+  products.filter(product => product.compare === true);
 
 export const getNew = ({ products }) =>
   products.filter(item => item.newFurniture === true);
@@ -10,10 +12,16 @@ export const getNew = ({ products }) =>
 /* actions */
 const createActionName = actionName => `app/products/${actionName}`;
 const TOGGLE_PRODUCT_FAVORITE = createActionName('TOGGLE_PRODUCT_FAVORITE');
+const TOGGLE_PRODUCT_COMPARE = createActionName('TOGGLE_PRODUCT_COMPARE');
 
 /* action creators */
 export const toggleFavoriteProduct = payload => ({
   type: TOGGLE_PRODUCT_FAVORITE,
+  payload,
+});
+
+export const toggleProductCompare = payload => ({
+  type: TOGGLE_PRODUCT_COMPARE,
   payload,
 });
 
@@ -24,6 +32,12 @@ export default function reducer(statePart = [], action = {}) {
       return statePart.map(product =>
         product.id === action.payload
           ? { ...product, isFavorite: !product.isFavorite }
+          : product
+      );
+    case TOGGLE_PRODUCT_COMPARE:
+      return statePart.map(product =>
+        product.id === action.payload
+          ? { ...product, compare: !product.compare }
           : product
       );
     default:
