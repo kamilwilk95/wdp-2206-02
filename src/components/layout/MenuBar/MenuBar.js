355ced1b79f90f0de97dadb39d118d154a820840
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-06-12 22:52:31 +0300

    Add Menu bar doesn't hide in the nav-toggle button

--- src/components/layout/MenuBar/MenuBar.js
+++ src/components/layout/MenuBar/MenuBar.js
@@ -10,36 +10,55 @@ const MenuBar = ({ children }) => (
   <div className={styles.root}>
     <div className='container'>
       <div className='row align-items-center'>
-        <div className={'col order-1 order-xs-2'}>
+        <div className={'col order-sm-2'}>
           <ProductSearch />
         </div>
-        <div className={clsx('col-auto order-2 order-xs-1', styles.menu)}>
-          <ul>
-            <li>
-              <a href='#' className={styles.active}>
-                Home
-              </a>
-            </li>
-            <li>
-              <a href='#'>Furniture</a>
-            </li>
-            <li>
-              <a href='#'>Chair</a>
-            </li>
-            <li>
-              <a href='#'>Table</a>
-            </li>
-            <li>
-              <a href='#'>Sofa</a>
-            </li>
-            <li>
-              <a href='#'>Bedroom</a>
-            </li>
-            <li>
-              <a href='#'>Blog</a>
-            </li>
-          </ul>
-        </div>
+
+        <nav className='navbar navbar-expand-sm bg-light' id='mobile-menu-wrapper'>
+          <div className='container-fluid'>
+            <button
+              className='navbar-toggler'
+              type='button'
+              data-bs-toggle='collapse'
+              data-bs-target='#mobile-menu'
+              aria-controls='mobile-menu'
+              aria-expanded='false'
+              aria-label='Toggle navigation'
+            >
+              <span className='navbar-toggler-icon'></span>
+            </button>
+            <div
+              className={clsx('col-auto order-sm-1 collapse', styles.menu)}
+              id='mobile-menu'
+            >
+              <ul className='navbar-nav'>
+                <li className='nav-item'>
+                  <a href='#' className={styles.active}>
+                    Home
+                  </a>
+                </li>
+                <li className='nav-item'>
+                  <a href='#'>Furniture</a>
+                </li>
+                <li className='nav-item'>
+                  <a href='#'>Chair</a>
+                </li>
+                <li className='nav-item'>
+                  <a href='#'>Table</a>
+                </li>
+                <li>
+                  <a href='#'>Sofa</a>
+                </li>
+                <li className='nav-item'>
+                  <a href='#'>Bedroom</a>
+                </li>
+                <li className='nav-item'>
+                  <a href='#'>Blog</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </div>
     </div>
   </div>
