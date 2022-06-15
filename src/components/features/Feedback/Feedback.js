309603062a9d4e1bc086bfed526e0a68bc46e6ca
Slawomir Johansen <baran@fastmail.com>
2022-06-15 18:24:26 +0200

    ADD client feedback component template

--- src/components/features/Feedback/Feedback.js
+++ src/components/features/Feedback/Feedback.js
@@ -10,7 +10,7 @@ const Feedback = () => {
             <div className={'col-auto ' + styles.heading}>
               <h3>Client Feedback</h3>
             </div>
-            <div className={'col ' + styles.menu}></div>
+            <div className={'col'}></div>
             <div className={'col-auto ' + styles.dots}>
               <ul>
                 <li>
@@ -22,13 +22,35 @@ const Feedback = () => {
                 <li>
                   <a className={''}>page</a>
                 </li>
-                <li>
-                  <a className={''}>page</a>
-                </li>
-                <li>
-                  <a className={''}>page</a>
-                </li>
               </ul>
+            </div>
+          </div>
+        </div>{' '}
+        <div className='row justify-content-center mb-3'>
+          <img src='/images/apostrophe.png' alt='apostrophe' />
+        </div>
+        <div className='row justify-content-center'>
+          <div className={'col-10'}>
+            <div className={styles.feedbackContent}>
+              Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+              labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+              accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren.
+            </div>
+          </div>
+        </div>
+        <div className={styles.feedbackAuthorWrapper}>
+          <div className='row justify-content-center'>
+            <div className='col-2'>
+              <div className={styles.authorBox}></div>
+            </div>
+            <div className='col-2'>
+              <div className='row'>
+                {' '}
+                <div className={styles.feedbackAuthorText}> John Smith </div>{' '}
+              </div>
+              <div className='row'>
+                <div className={styles.feedbackAuthorTitle}> Furniture Client </div>
+              </div>
             </div>
           </div>
         </div>
