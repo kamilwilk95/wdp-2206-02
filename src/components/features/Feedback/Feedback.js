30c53f6ff6a1ae4e234c248260b5f13532caf797
Slawomir Johansen <baran@fastmail.com>
2022-06-18 14:30:26 +0200

    ADD Swipe functionality

--- src/components/features/Feedback/Feedback.js
+++ src/components/features/Feedback/Feedback.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import styles from './Feedback.module.scss';
 import { getAllFeedback, getFeedbackCount } from '../../../redux/feedbackRedux';
 import { useSelector } from 'react-redux';
+import Swipe from '../../common/Swipe/Swipe';
 
 const Feedback = () => {
   const [activePage, setActivePage] = useState(0);
@@ -28,7 +29,7 @@ const Feedback = () => {
   }
 
   //placeholder for Swipe functionality
-  /* const rightAction = () => {
+  const rightAction = () => {
     setActivePage(activePage - 1);
 
     if (activePage <= 0) {
@@ -43,53 +44,56 @@ const Feedback = () => {
       setActivePage(activePage);
     }
   };
-  */
 
   return (
-    <div className={styles.root}>
-      <div className='container'>
-        <div className={styles.panelBar}>
-          <div className='row no-gutters align-items-end'>
-            <div className={'col-auto ' + styles.heading}>
-              <h3>Client Feedback</h3>
-            </div>
-            <div className='col'></div>
-            <div className={'col-auto ' + styles.dots}>
-              <ul>{dots}</ul>
-            </div>
-          </div>
-        </div>
-        <div className='row justify-content-center'>
-          <span className={'font-secular ' + styles.apostrophe}> &apos;&apos;</span>
-        </div>
-        {feedbackData.slice(activePage, activePage + 1).map(item => (
-          <div key={item.id}>
-            <div className='row justify-content-center'>
-              <div className='col-10'>
-                <div className={styles.feedbackContent}>{item.content}</div>
+    <Swipe leftAction={leftAction} rightAction={rightAction}>
+      <div className={styles.root}>
+        <div className='container'>
+          <div className={styles.panelBar}>
+            <div className='row no-gutters align-items-end'>
+              <div className={'col-auto ' + styles.heading}>
+                <h3>Client Feedback</h3>
+              </div>
+              <div className='col'></div>
+              <div className={'col-auto ' + styles.dots}>
+                <ul>{dots}</ul>
               </div>
             </div>
-            <div className={styles.feedbackAuthorWrapper}>
+          </div>
+          <div className='row justify-content-center'>
+            <span className={'font-secular ' + styles.apostrophe}> &apos;&apos;</span>
+          </div>
+          {feedbackData.slice(activePage, activePage + 1).map(item => (
+            <div key={item.id}>
               <div className='row justify-content-center'>
-                <div className='col-2'>
-                  <div className={styles.authorBox}>
-                    <img src={item.photo} alt='apostrophe' />
-                  </div>
+                <div className='col-10'>
+                  <div className={styles.feedbackContent}>{item.content}</div>
                 </div>
-                <div className='col-2'>
-                  <div className='row'>
-                    <div className={styles.feedbackAuthorText}> {item.author} </div>
+              </div>
+              <div className={styles.feedbackAuthorWrapper}>
+                <div className='row justify-content-center'>
+                  <div className='col-2'>
+                    <div className={styles.authorBox}>
+                      <img src={item.photo} alt='apostrophe' />
+                    </div>
                   </div>
-                  <div className='row'>
-                    <div className={styles.feedbackAuthorTitle}>{item.authorTitle}</div>
+                  <div className='col-2'>
+                    <div className='row'>
+                      <div className={styles.feedbackAuthorText}> {item.author} </div>
+                    </div>
+                    <div className='row'>
+                      <div className={styles.feedbackAuthorTitle}>
+                        {item.authorTitle}
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </Swipe>
   );
 };
 
