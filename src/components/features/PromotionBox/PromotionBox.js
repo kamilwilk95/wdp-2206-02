923536786798b87cc4161b7d4a0190302a030b74
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-06-19 23:24:00 +0300

    Add flexbox instead bootstrap

--- src/components/features/PromotionBox/PromotionBox.js
+++ src/components/features/PromotionBox/PromotionBox.js
@@ -1,34 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import styles from './PromotionBox.module.scss';
+
 const PromotionBox = promotion_img => {
   return (
     <div>
       <div className='container'>
-        <div className='row'>
-          <div className='col-6'>
-            <img className='w-100' src='./images/promotion-bed.jpg' alt='promotion' />
-            <div className='text-black rounded-3'>
-              <h2>
-                GUEST ROOM<span> SOFA</span>
-              </h2>
-              <h3>-20%</h3>
+        <div className={styles.wrapper}>
+          <div className={styles.leftinner}>
+            <div className={styles.box_img}>
+              <img src='./images/promotion-bed.jpg' alt='promotion-left' />
             </div>
+
+            <p className={styles.promo_text}>
+              GUEST ROOM<span> SOFA</span>
+            </p>
+            <p className={styles.text_price}>-20%</p>
           </div>
-          <div className='col-6'>
-            <div className='row'>
-              <div className='col-12'>
-                <img
-                  className='w-100 h-25'
-                  src='./images/promotion-bed.jpg'
-                  alt='promotion'
-                />
-                <img
-                  className='w-100 h-25'
-                  src='./images/promotion-bed.jpg'
-                  alt='promotion'
-                />
-              </div>
+          <div className={styles.rightinner}>
+            <div className={styles.top_product}>
+              <img src='./images/promotion-bed.jpg' alt='promotion' />
+              <p>
+                GUEST ROOM<span> SOFA</span>
+              </p>
+              <p>-20%</p>
+            </div>
+            <div className={styles.bottom_product}>
+              <img src='./images/promotion-bed.jpg' alt='promotion' />
+              <p>
+                GUEST ROOM<span> SOFA</span>
+              </p>
+              <p>-20%</p>
             </div>
           </div>
         </div>
