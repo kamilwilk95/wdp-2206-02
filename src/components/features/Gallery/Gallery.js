28dbb82a827acb8be37a75bb0fb4e69751de9d5b
Jakub Kozik <jakubkozik120@gmail.com>
2022-06-24 11:53:40 +0200

    Fix images

--- src/components/features/Gallery/Gallery.js
+++ src/components/features/Gallery/Gallery.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useState } from 'react';
@@ -47,7 +48,7 @@ const Gallery = () => {
 
   useEffect(() => {
     setActiveProduct(categoryProducts[0].id);
-  }, [activeCategory, categoryProducts]);
+  }, [activeCategory]);
 
   const handleProductChange = (e, newProduct) => {
     e.preventDefault();
